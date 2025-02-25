2f9a56aa5d6e2aa33682b55d246d336d427be696
Josh S <josh@operationspark.org>
2025-02-25 17:03:00 -0600

    (feat) aggregating ave of base attack of each type in ascending order

--- technical/challenge-02/aggregate.js
+++ technical/challenge-02/aggregate.js
@@ -3,6 +3,8 @@ import { getDb } from './db.js';
 const pokemon = getDb().collection('pokemon');
 
 const aggCursor = pokemon.aggregate([
+  
+  
   { $facet: {
     grassPokemonHP: [
         { $match: {
@@ -17,17 +19,25 @@ const aggCursor = pokemon.aggregate([
       { $unwind: '$type'},
 
   { $group: {
-    _id: '$type',
+    _id: {
+      type:'$type'},
     totalOfEachType: {$sum: 1}
   }
   },
   { $sort: {totalOfEachType: -1}}
+    ],
+    baseAttackAve: [
+      { $unwind: '$type'},
+  { $group: {
+    _id: {
+      type: '$type',
+    },
+    baseAttackAve: {$avg: '$base.Attack'},
+  }},
+  { $sort: { baseAttackAve: 1}}
     ]
   }},
-  
-  // { $sort: {
-  //   totalOfEachType: -1 
-  // }}
+
   // Your aggregation stages go here
 ]);
 
