da084a9434d798c62dca37dcefe3b97d418633aa
Josh S <josh@operationspark.org>
2025-02-25 18:31:02 -0600

    (feat) 3 upgrades - agg of first letters in desc order, agg of top 4 fastest pokemon types, agg of ave of all base stats for each type in desc order

--- technical/challenge-02/aggregate.js
+++ technical/challenge-02/aggregate.js
@@ -3,8 +3,6 @@ import { getDb } from './db.js';
 const pokemon = getDb().collection('pokemon');
 
 const aggCursor = pokemon.aggregate([
-  
-  
   { $facet: {
     grassPokemonHP: [
         { $match: {
@@ -19,8 +17,7 @@ const aggCursor = pokemon.aggregate([
       { $unwind: '$type'},
 
   { $group: {
-    _id: {
-      type:'$type'},
+    _id: '$type',
     totalOfEachType: {$sum: 1}
   }
   },
@@ -29,13 +26,40 @@ const aggCursor = pokemon.aggregate([
     baseAttackAve: [
       { $unwind: '$type'},
   { $group: {
-    _id: {
-      type: '$type',
-    },
+    _id: '$type',
+    
     baseAttackAve: {$avg: '$base.Attack'},
   }},
   { $sort: { baseAttackAve: 1}}
-    ]
+    ],
+    totalByLetter: [
+      { $group: {
+        _id: { $substr: [ '$name.english', 0, 1]},
+          totalByLetter: { $sum: 1}
+      }},
+      { $sort: { totalByLetter: -1}}
+    ],
+    fastestPokemon: [
+      { $unwind: '$type'},
+  { $group: {
+    _id: '$type',
+    speedAve: { $avg: '$base.Speed'}
+  }},
+  { $match: {
+    speedAve: { $gte: 80}
+  }},
+  { $sort: { speedAve: -1}}
+    ],
+    baseStatAverage: [{ $unwind: '$type'},
+    { $project: {
+      _id: '$type',
+    baseStatAve: {$divide: [{$add: ['$base.HP', '$base.Attack', '$base.Defense', '$base.Speed']}, 4]}
+    }},
+    { $group: {
+      _id: '$_id',
+      baseStatAve: {$avg: '$baseStatAve'}
+    }},
+    { $sort: { baseStatAve: -1}}]
   }},
 
   // Your aggregation stages go here
